dc0bfecdca2bac90b4c5bd00861710256bbd345b
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-02-23 13:46:23 -0300

    messup

---
+++ lesson-3/src/example.js
@@ -0,0 +1,10 @@
+const username = 'freddy';
+typeof username === 'string';
+
+if (!(ServiceWorker in navigator)) {
+  // you have an old browser
+}
+
+const greeting = 'hello';
+console.log(`${greeting} world`);
+[1, 2, 3].forEach((x) => console.log(x));
